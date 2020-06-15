e327321e38bd2312eded580379920325202304fd
darlson <davertiser@gmail.com>
2020-06-15 14:05:28 -0600

    end of morning review

--- server/controllers/moviesController.js
+++ server/controllers/moviesController.js
@@ -1,9 +1,30 @@
 module.exports = {
   getAllMovies: async (req, res) => {
     const db = req.app.get('db')
+    const { rating, year } = req.query
+   
     const movies = await db.movies.find()
 
-    res.status(200).send(movies)
+    // generalize for multiple queries
+    // const filteredMovies = movies.filter(movie => {
+    //   let include = false
+    //   for(let key in req.query){
+    //     if(movie[key] > +req.query[key]){
+    //       include = true
+    //     }
+    //   }
+    //   return include
+    // })
+    // res.status(200).send(movies)
+
+    if(rating) {
+      const filteredMovies = movies.filter(movie => {
+        return movie.rating >= +rating
+      })
+      res.status(200).send(filteredMovies)
+    } else {
+      res.status(200).send(movies)
+    }
   },
   getMovieById: async (req, res) => {
     const db = req.app.get('db')
--- server/index.js
+++ server/index.js
@@ -1,22 +1,32 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
 const massive = require('massive')
-const { SERVER_PORT, CONNECTION_STRING } = process.env
+const session = require('express-session')
+const app = express()
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
 const setup = require('./controllers/setup')
 const authCtrl = require('./controllers/authController')
 const carCtrl = require('./controllers/carController')
 const movieCtrl = require('./controllers/moviesController')
+const authMiddleware = require('./middlewares/authMiddleware')
+const {USER, ADMIN} = require('../constants/ROLES')
 
 app.use(express.json())
-
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: true,
+        secret: SESSION_SECRET,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }
+    })
+)
 /**
  * ! MASSIVE AND QUERIES TODOS
- * TODO: Set up heroku db/.env
- * TODO: Set up massive connection
- * TODO: Build controllers
- * TODO: - Cars controller(db folder/object syntax)
- * TODO: - Movies controller(inline)
+ * //TODO: Set up heroku db/.env
+ * //TODO: Set up massive connection
+ * //TODO: Build controllers
+ * //TODO: - Cars controller(db folder/object syntax)
+ * //TODO: - Movies controller(inline)
  * TODO: Set up queries
  * TODO: - Filter cars by make, model, year, color
  * TODO: - Filter movies by year, rating
@@ -24,7 +34,7 @@ app.use(express.json())
 
 /**
  * ! SESSION & MIDDLEWARE TODOS
- * TODO: Set up session
+ * //TODO: Set up session
  * TODO: - Modify auth functions to use session
  * TODO: Build useful middleware
  * TODO: - Auth middleware
@@ -33,10 +43,10 @@ app.use(express.json())
  */
 
 //* Movie Endpoints
-app.get('/api/movies', movieCtrl.getAllMovies)
-app.get('/api/movies/:id', movieCtrl.getMovieById)
-app.post('/api/movies', movieCtrl.addMovie)
-app.delete('/api/movies/:id', movieCtrl.deleteMovie)
+app.get('/api/movies', authMiddleware([USER, ADMIN]), movieCtrl.getAllMovies)
+app.get('/api/movies/:id', authMiddleware([USER, ADMIN]), movieCtrl.getMovieById)
+app.post('/api/movies', authMiddleware([ADMIN]), movieCtrl.addMovie)
+app.delete('/api/movies/:id', authMiddleware([ADMIN]), movieCtrl.deleteMovie)
 
 //* Car endpoints
 
@@ -49,6 +59,9 @@ app.delete('/api/cars/:id', carCtrl.deleteCar)
 app.post('/auth/login', authCtrl.login)
 app.post('/auth/register', authCtrl.register)
 app.delete('/auth/logout', authCtrl.logout)
+
+//* Users endpoints
+// app.get('/api/users/:role_id', userCtrl.something)
 
 //! Seeding endpoint.  Keep at bottom.
 app.post('/api', setup.seed)
